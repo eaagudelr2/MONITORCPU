b3d5ffc909c3b5b20081fd2e9194b3420ef42405
eaagudelr2 <132963946+eaagudelr2@users.noreply.github.com>
2024-06-03 13:08:55 -0500

    Update alerta.sh

--- alerta.sh
+++ alerta.sh
@@ -6,7 +6,7 @@
   #--header 'account: 111111111111' \
   #--header 'apikey: xxxxxxxxxxxxxxxxxxxxx' \
   #--header 'token: xxxxxxxxxxxxxxxxxxxxxxxxxx ' \
-  #--body-data '{"toNumber":"573148791970","sms":"El CPU% esta alto","flash":0,"sc":"890202","request_dlvr_rcpt":0}' \
+  #--body-data '{"toNumber":"5731488888888","sms":"El CPU% esta alto","flash":0,"sc":"890202","request_dlvr_rcpt":0}' \
   #--output-document \
   #- https://api103.hablame.co/api/sms/v3/send/priority
 
